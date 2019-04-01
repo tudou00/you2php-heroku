896f7c8b6c57a3dda86294b858298a5949ff86c8
tudou00 <49158722+tudou00@users.noreply.github.com>
2019-04-01 22:30:51 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,9 +1,9 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', getenv("APIKEY"));
-define('GJ_CODE', getenv("GJ_CODE"));
-define('SITE_NAME',getenv("SITE_NAME"));
-define('TITLENAME', getenv("TITLENAME"));
-define('EN2DEKEY',getenv("EN2DEKEY"));
-define('EMAIL', getenv("EMAIL"));
+define('APIKEY', 'AIzaSyB_GUwfw1KiThu1JdxLvzJu71423i-Po9E');
+define('GJ_CODE', 'US');
+define('SITE_NAME','tube');
+define('TITLENAME', 'tube');
+define('EN2DEKEY','aasdioeekkdlsfeefe');
+define('EMAIL', '123@456.com');
 ?>
